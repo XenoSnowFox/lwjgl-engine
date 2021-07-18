3a6cb36493b992a0085590c34f55379755599785
Xeno Snow Fox <xenosnowfox@gmail.com>
2021-07-18 14:26:17 +1000

    Update ObjectLibraryStreamReader.java

--- src/main/java/com/xenosnowfox/lwjglengine/render/object/ObjectLibraryStreamReader.java
+++ src/main/java/com/xenosnowfox/lwjglengine/render/object/ObjectLibraryStreamReader.java
@@ -2,7 +2,11 @@ package com.xenosnowfox.lwjglengine.render.object;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayDeque;
 import java.util.Objects;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ObjectLibraryStreamReader {
 
@@ -10,16 +14,26 @@ public class ObjectLibraryStreamReader {
 
 	private boolean finished = false;
 
+	private Queue<Float> vertex = new ArrayDeque<>();
+
 	public ObjectLibraryStreamReader(final InputStream withInputStream) {
 		this.inputStream = Objects.requireNonNull(withInputStream);
 	}
 
+	@Override
+	public String toString() {
+		return this.getClass().getName() + "{" + System.lineSeparator()
+				+ "\tvertex=" + vertex.toString() + System.lineSeparator()
+				+ "}";
+	}
+
 	public ObjectLibrary read() throws IOException {
-		String line;
-		do {
-			line = this.readLine();
+		String line = this.readLine();
+		while (line != null) {
 			System.out.println(line);
-		} while (line != null);
+			line = this.readLine();
+		}
+		System.out.println(this.toString());
 		return null;
 	}
 
@@ -35,7 +49,26 @@ public class ObjectLibraryStreamReader {
 			b = this.inputStream.read();
 		}
 		this.finished = b < 0;
-		return sb.toString();
+		return sb.toString().trim();
 	}
 
+
+	private void parseLine(String withLine) throws IOException  {
+		String[] parts = withLine.trim()
+				.split(" ", 2);
+
+		if (parts[0].equalsIgnoreCase("v")) {
+			this.parseVertex(parts[1].split(" "));
+		}
+	}
+
+	private void parseVertex(String... values) throws IOException {
+		var parts = Stream.of(values)
+				.filter(s -> !s.isBlank())
+				.collect(Collectors.toList());
+
+		if (parts.size() != 3) {
+			throw new IOException("Vertex must contain exactly 3 parts.");
+		}
+	}
 }
